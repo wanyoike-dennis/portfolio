5e2608b535bbb142e35f2a9d339664c66a6cc2f1
Deehknow <43774793+Deehknow@users.noreply.github.com>
2024-11-27 12:58:18 +0300

    speedInsights added

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+    
     </BrowserRouter>
   </React.StrictMode>
 );
